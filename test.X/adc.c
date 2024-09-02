c630fa509163ac39d7750fef120ccf41cb74e1ce
hala20110 <162651183+hala20110@users.noreply.github.com>
2024-09-02 15:38:23 +0300

    adc.c

--- test.X/adc.c
+++ test.X/adc.c
@@ -9,18 +9,19 @@
 #include <xc.h>
 
 // adc.c
-#include <avr/io.h>
-#include "adc.h"
 
-void ADC_init(void) {
-    // Initialize ADC
-    ADMUX = (1 << REFS0);  // Reference voltage
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);  // Enable ADC and set prescaler
+#include "adc.h"
+#include <avr/io.h>
+
+void init_adc(void) {
+    // Set ADC prescaler and enable ADC
+    ADMUX = (1 << REFS0); // Reference voltage on AVCC
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); // ADC Enable and prescaler of 64
 }
 
-uint16_t ADC_read(uint8_t channel) {
+uint16_t read_adc(uint8_t channel) {
     ADMUX = (ADMUX & 0xF0) | (channel & 0x0F); // Select ADC channel
-    ADCSRA |= (1 << ADSC);  // Start conversion
-    while (ADCSRA & (1 << ADSC)); // Wait for conversion to finish
+    ADCSRA |= (1 << ADSC); // Start conversion
+    while(ADCSRA & (1 << ADSC)); // Wait for conversion to complete
     return ADC;
 }
