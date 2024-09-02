c358e6b5755fc48fb8ab00eb4bf463a690910f5f
hala20110 <162651183+hala20110@users.noreply.github.com>
2024-09-02 15:44:10 +0300

    motor.c

---
+++ test.X/motor.c
@@ -0,0 +1,15 @@
+
+
+
+// motor.c
+
+#include "motor.h"
+
+// This is a placeholder for the actual calculation based on encoder data
+uint8_t calculate_rpm() {
+    // Example calculation
+    uint8_t rpm = 0;
+    // Add code here to calculate RPM from encoder data
+    return rpm;
+}
+
