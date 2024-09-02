b8d10e6b5efc3943149d19f4838e855f7d8e95f7
hala20110 <162651183+hala20110@users.noreply.github.com>
2024-09-02 15:45:11 +0300

    motor.h

---
+++ test.X/motor.h
@@ -0,0 +1,31 @@
+/*
+ * File:   interrupt.c
+ * Author: kareemaayman
+ *
+ * Created on September 1, 2024, 1:45 PM
+ */
+/* 
+ * File:   motor.h
+ * Author: Hala
+ *
+ * Created on September 1, 2024, 10:55 PM
+ */
+
+#ifndef MOTOR_H
+#define	MOTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+uint8_t calculate_rpm();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOTOR_H */
+
